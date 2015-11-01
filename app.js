748284d6d9e5a431d767dd9199b276f5afe53e64
WeiZhen <zhenw@WeiZhens-MacBook-Air.local>
2015-10-31 20:35:49 -0400

    menu_module has been set up ready to add pages

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var menuBuilder = require('./routes/menu_module/menuBuilder.js');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/menu_builder', menuBuilder);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
